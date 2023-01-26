6c51483d21da4fe18673fbd8cdc8380377c80447
michaelsatumba <mjsatumba@gmail.com>
2023-01-26 09:38:49 -0800

    Login

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-#browser = webdriver.Chrome()
+browser = webdriver.Chrome()
 
-#browser.get('https://www.wyzant.com/login')
+browser.get('https://www.wyzant.com/login')
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,4 +9,16 @@ load_dotenv()
 import os
 #print(os.environ)
 nameInsert = os.getenv("NAME")
-print(nameInsert)
+passwordInsert = os.getenv("PASSWORD")
+# print(nameInsert)
+# print(passwordInsert)
+
+username = browser.find_element("xpath", '/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[8]/form/div[1]/input')
+username.send_keys(nameInsert)
+
+
+password = browser.find_element("xpath", '/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[8]/form/div[2]/input')
+password.send_keys(passwordInsert)
+
+loginButton = browser.find_element("xpath", "/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[8]/form/button")
+loginButton.click()
