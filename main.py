1376d4824e83300386044f8b6bee5993bdd0af9f
michaelsatumba <mjsatumba@gmail.com>
2023-01-31 19:13:11 -0800

    added print statements - easy for debug

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Description: This script will automatically apply to jobs on Wyzant.com
+import sys
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,21 +19,36 @@ def login(browser, username, password):
     browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[8]/form/div[1]/input').send_keys(username)
     browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[8]/form/div[2]/input').send_keys(password)
     browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[1]/div/div/div/div/div[8]/form/button").click()
+    print("Logged in")
 
 # Go to jobs page function
 def go_to_jobs_page(browser, wait):
     wait.until(EC.presence_of_element_located((By.ID, "jobs-widget"))).click()
+    print("Clicked jobs widget")
 
 # Click job details function
 def click_job_details(browser, wait):
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "job-details-link"))).click()
+    try:
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "job-details-link"))).click()
+        print("Clicked job details")
+    except:
+        print("No more jobs.")
+        sys.exit()
+
 
 # Select subject function
 def select_subject(browser, wait):
-    subject = wait.until(EC.presence_of_element_located((By.TAG_NAME, "h1"))).text
+    subject_one = wait.until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
+    subject_text = subject_one.text
     select_element = wait.until(EC.presence_of_element_located((By.ID, "template_select")))
-    Select(select_element).select_by_visible_text(subject)
+    choose = Select(select_element)
 
+    try:
+        choose.select_by_visible_text(subject_text)
+        print("selected apprpriate template")
+    except NoSuchElementException:
+        print("No option with text '{}' found in the select tag.".format(subject_text))
+        
 # Get name and format it function
 def get_name(browser, wait):
     name = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#wyzantResponsiveColumns > div.columns.medium-8.small-12 > h4"))).text
@@ -40,14 +57,21 @@ def get_name(browser, wait):
     current_text = text_area.get_attribute("value")
     text_area.clear()
     text_area.send_keys(f"Hello {formatted_name}! " + current_text)
+    print("Got name and formatted it")
 
 # Check and click checkbox function
 def check_and_click_checkbox(browser, wait):
     try:
-        checkbox = wait.until(EC.presence_of_element_located((By.ID, "agree_partner_hourly_rate")))
+        # checkbox = wait.until(EC.presence_of_element_located((By.ID, "agree_partner_hourly_rate")))
+        # checkbox = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/form/div[5]/div[3]/input")))
+        checkbox = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#agree_partner_hourly_rate")))
+        print("Checkbox found.")
         checkbox.click()
+        print("Checkbox clicked.")
     except:
+        print("Checkbox not found.")
         pass
+  
 
 # Submit application function
 def submit_application(browser, wait):
@@ -62,16 +86,13 @@ go_to_jobs_page(browser, wait)
 
 # Loop through jobs
 for i in range(50):
-    try:
-        click_job_details(browser, wait)
-        select_subject(browser, wait)
-        get_name(browser, wait)
-        check_and_click_checkbox(browser, wait)
-        submit_application(browser, wait)
-        print("Applied to job", i + 1)
-    except:
-        print("No more jobs to apply for.")
-        break
+    click_job_details(browser, wait)
+    select_subject(browser, wait)
+    get_name(browser, wait)
+    check_and_click_checkbox(browser, wait)
+    submit_application(browser, wait)
+    print("Applied to job", i + 1)
+
 
 
 
